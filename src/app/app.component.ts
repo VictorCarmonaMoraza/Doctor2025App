b0bb9cfc9380d81cc0fd8c9b5c6ddabf5c39a80d
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2025-03-07 09:12:41 +0100

    Petciones http en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,18 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'doctor2025App';
+  users: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5065/api/User').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('La solicitud esta completa')
+    })
+  }
+
 }
