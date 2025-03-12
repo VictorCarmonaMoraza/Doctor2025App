a6e0e61ab49a8bdd8cc224bd075c7f2975aa3f9a
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2025-03-12 07:47:56 +0100

    34-Utilizar servicio Usuario para el Login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { UserService } from '../services/user.service';
+import { SharedService } from '../../shared/services/shared.service';
+import { Login } from '../interfaces/login';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +13,47 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  formLogin: FormGroup;
+  hiddenPassword: boolean = true;
+  showLoading: boolean = false;
+
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private userService: UserService,
+    private sharedService: SharedService
+  ) {
+    this.formLogin = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  /**
+   * Iniciar sesion en Login
+   */
+  startSesion() {
+    this.showLoading = true;
+    const request: Login = {
+      username: this.formLogin.value.username,
+      password: this.formLogin.value.password
+    }
+    this.userService.startSesion(request).subscribe({
+      next: (resp) => {
+        this.sharedService.saveSession(resp);
+        this.router.navigate(['layout'])
+      },
+      complete: () => {
+        this.showLoading = false;
+      },
+      error: (error) => {
+        this.sharedService.showAlerts(error.error, 'Error!');
+        this.showLoading = false;
+      }
+    })
+  }
+
 
 }
